36baece77cb477d3f51a58a17d7a16c162f4bdf7
MiluchOK <milyukov.alexey@gmail.com>
2018-03-02 00:15:28 -0800

    Breaking up by containers and components

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import Main from './containers/Main';
 
 class App extends Component {
   
   render() {
     return (
       <div className="App">
-        <p>Stuff</p>
-        <p>Lets trigger some action MTF</p>
+        <Main />
       </div>
     );
   }
---
+++ client/src/containers/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Main extends Component {
+  
+  render() {
+    return (
+      <div>
+        <p>Some stuff from main</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
