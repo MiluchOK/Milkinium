4a116774e614f047bb0bfcb8278bd3b85698abca
MiluchOK <milyukov.alexey@gmail.com>
2018-03-26 23:05:44 -0700

    Adding a new case button

--- client/src/screens/Cases.js
+++ client/src/screens/Cases.js
@@ -2,27 +2,68 @@ import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
 import { connect } from 'react-redux';
 import List from 'material-ui/List';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
 import Grid from 'material-ui/Grid';
+import AddIcon from 'material-ui-icons/Add';
 import Execution from './../components/ExecutionRow';
+
+const styles = theme => ({
+    root: {
+        backgroundColor: theme.palette.background.paper,
+        position: 'relative',
+    },
+
+    fab: {
+        position: 'fixed',
+        zIndex: 10000,
+        bottom: 30,
+        right: 20,
+    },
+});
 
 class Cases extends Component {
 
+    handleAddCase(){
+        console.log("Adding a new case!")
+    }
+
     render() {
 
+        const { classes, theme } = this.props;
+
         const cases = [
-            {}, {}, {}
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
         ];
 
         return (
+
             <Grid style={{flex: 1}}>
+                <div className={classes.root}>
                 <List component="nav">
                     {cases.map((e => (
                         <Execution />
                     )))}
                 </List>
+
+                <Button variant="fab"
+                        onClick={this.handleAddCase}
+                        color="primary"
+                        aria-label="add"
+                        className={classes.fab}>
+                    <AddIcon />
+                </Button>
+                </div>
             </Grid>
         );
     }
 }
 
-export default Cases;
+export default withStyles(styles, { withTheme: true })(Cases);
